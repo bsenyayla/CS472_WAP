219fc0a19d4d1ce68df116bfd975b3787c7e87ef
bsenyayla <56520710+bsenyayla@users.noreply.github.com>
2022-05-30 12:17:44 -0500

    ok

---
+++ Lesson04_Table/script00.js
@@ -0,0 +1,17 @@
+console.log("hello javascript");
+
+
+
+
+
+
+/* JAva 2 */
+console.log("External File");
+var i=9;
+i="HEllo js";
+console.log(i);
+
+//document.getElementById("helloText").innerText="tessfd";
+//helloText.innerText="welcome to dom api";
+
+console.log(this);
