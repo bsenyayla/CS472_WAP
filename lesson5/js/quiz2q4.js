bde360649b863a9538f4c895ecaa3d9c4502f1de
bsenyayla <56520710+bsenyayla@users.noreply.github.com>
2022-06-03 00:44:44 -0500

    ok

---
+++ lesson5/js/quiz2q4.js
@@ -0,0 +1,15 @@
+console.log(printName("Bahadır ŞENYAYLA"));
+
+var printName= function(name){
+    return "Hello -> " + name;
+}
+
+var sampleFunc=function(a,b){
+    return "Toplam:" + (a+b);
+}
+
+
+
+console.log(
+    sampleFunc(5,6)
+);
