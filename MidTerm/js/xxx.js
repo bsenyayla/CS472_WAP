e8c843c5b440d5fa58ad7b13c142e959d37c0e8e
bsenyayla <56520710+bsenyayla@users.noreply.github.com>
2022-06-08 22:59:29 -0500

    ok

--- MidTerm/js/xxx.js
+++ MidTerm/js/xxx.js
@@ -1,3 +1,5 @@
+"use strict"
+
 const address={
 street:"NORTH 4th street",
 city:"Fairfield"
@@ -8,3 +10,23 @@ var aa='Test';
 console.log(`sdf   ${address.city}`);
 
 
+function foo3(){
+    this.prop="hello";  //strict modda hata verir
+}
+//foo3();
+/*
+console.log(prop);
+console.log(this);
+console.log(this.prop);
+*/
+
+function foo4(){
+    const PI=3.14;
+    function bar(){
+        console.log(this);
+    }
+    //bar();
+    console.log(this);
+}
+
+foo4();
